d6c291b872f8c795f9516b9a823024a579cc3ccd
Sai Pavan Nibhanupudi <saipavan@Sais-MacBook-Pro.local>
2025-09-03 23:12:21 +0530

    Adding employees, printing employees info

--- code/src/main.C
+++ code/src/main.C
@@ -7,17 +7,28 @@ using namespace std;
 #include"ExtDeque.H"
 #include"XyzUtils.H"
 #include"XyzEmployee.H"
+#include"XyzEmpManager.H"
 
 
 int main() {
 	cout << "Hello world..!" << endl;
+	//XyzEmployee a(1,1);
+	//cout << a;
 	//XyzFullTimeEmployee x("a", "b", "c", "d");
-	XyzFullTimeEmployee x(1);
-	cout << x << endl;
-	XyzContractorEmployee y(2);
-	cout << y << endl;
-	XyzInternEmployee z(3);
-	cout << z << endl;
+	//XyzFullTimeEmployee x(1);
+	//cout << x << endl;
+	//XyzContractorEmployee y(2);
+	//cout << y << endl;
+	//XyzInternEmployee z(3);
+	//cout << z << endl;
+	//XyzEmployee * ptr = &x;
+	//cout << ptr->getTotalLeaves() << endl;
+	//XyzEmployeeIf * ptr2 = &x;
+	//cout << (*ptr2) << endl;
+	//XyzEmployee b(1,1);
+	//cout << b << endl;
+	//cout << ptr2->getTotalLeaves() << endl;
+	//cout << (*ptr) << endl;
 	//Deque<int> d1;
 	/*ExtDeque<int> d2(3);
 	d2.fwdTraverse();
@@ -34,5 +45,70 @@ int main() {
 		d2.fwdTraverse();
 	}
 	//d2.sample(val);*/
+	//ExtDeque<int> a;
+	//cout << a << endl;
+	/*XyzFullTimeEmployee a(1);
+	XyzEmpIfPtr ptr1 = &a;
+	ExtDeque<XyzEmpIfPtr> *ptr = NULL;
+	ptr = new ExtDeque<XyzEmpIfPtr>;
+	cout << (*ptr1) << endl;
+	ptr->addNodeAtInd(0,ptr1);
+	ptr->fwdTraverse();*/
+
+	//ExtDeque<XyzEmployeeIf> ed1;
+	//XyzEmployeeIf * If1 = new XyzFullTimeEmployee(1);
+	//ed1.addNodeAtInd(0,If1);
+
+	/*ExtDeque<XyzEmployeeIf *> ed1;
+	XyzFullTimeEmployee x(1);
+	cout << &x << endl;
+	ed1.addNodeAtInd(0,&x);
+	ed1.fwdTraverse(); // it is printing node addresses
+	cout << ed1[0] << endl; // we are getting XyzEmployeeIf * here
+
+	cout << *p << endl;
+	*/
+	//cout << (*ed1[0]) << endl;
+	//XyzEmpIfPtr p = &x;
+	//cout << *p << endl;
+
+	//XyzFullTimeEmployee x(1);
+	//XyzFullTimeEmployee y(2);
+	//XyzEmployeeIf * mdata1 = &x;
+	//cout << *mdata1 << endl;
+
+	//Deque
+	//Node<XyzEmployeeIf *> *mhead = NULL;
+	//Node<XyzEmployeeIf *> *mtail = NULL;
+
+	//Node
+	//Node<XyzEmployeeIf *> * mnext = NULL;
+	//Node<XyzEmployeeIf *> * mprev = NULL;
+	//XyzEmployeeIf * mdata = &x;
+
+	// ExtDeque
+	//ExtDeque<XyzEmployeeIf *> ed1;
+	//ed1.addNodeAtInd(0,&x);
+	//ed1.addNodeAtInd(1,&y);
+	//ed1.fwdTraverse();
+	//cout << ed1[0] << endl;
+	//cout << ed1[1] << endl;
+
+
+
+
+
+
+	// above code is previous code requied to test
+	unsigned int sNoOfEmps = RandEmpDataGen::getRandomNumber(4,10);
+	cout << "Number of Emps : " << sNoOfEmps << endl;
+	if(sNoOfEmps) {
+		XyzEmployeeManager sMgr(sNoOfEmps);
+		sMgr.processEmployees();
+	}
+	else {
+		XyzEmployeeManager sMgr;
+		sMgr.processEmployees();
+	}
 	return 0;
 }
